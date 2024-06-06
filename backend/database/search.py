e88f223c1234cbd5ac9a88168f943c533ab0c96b
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-06-06 19:33:12 +0200

    More svg html

--- backend/database/search.py
+++ backend/database/search.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request
 from backend import conn
-from numpy import argmax
 from backend.database.chessFunctions import convertRawMoves
 from backend.database.chessFunctions import findMostSimilarGames
 import re
